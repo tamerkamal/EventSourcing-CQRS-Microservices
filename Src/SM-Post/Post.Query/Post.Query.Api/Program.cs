d39f3a9acf205486e14bc4ce47783f175c036c5a
Tamer Abdelsalam <t.abdelsalam@easternenterprise.com>
2022-10-12 16:30:59 +0200

    adding DbContext

--- Src/SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ Src/SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,17 +1,30 @@
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Infrastructure.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Add services to the container
+
+Action<DbContextOptionsBuilder> configureDbContext = (o => o.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+builder.Services.AddDbContext<DatabaseContext>(configureDbContext);
+
+#region Singleton
+
+builder.Services.AddSingleton<DatabaseContextFactory>(new DatabaseContextFactory(configureDbContext));
+
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#endregion
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region Configure the HTTP request pipeline
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -25,3 +38,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+#endregion
