2b39ecccff89decca9a13a63674f1c9739fdb5d2
Tamer Abdelsalam <t.abdelsalam@easternenterprise.com>
2022-10-18 18:42:13 +0200

    added AddPostController and Response Dtos

---
+++ Src/SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/AddPostController.cs
@@ -0,0 +1,59 @@
+namespace Post.Cmd.Api.Controllers;
+
+using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Cmd.Api.Dtos;
+using Post.Common.Dtos;
+
+[ApiController]
+[Route("api/v1/{controller}")]
+public class AddPostController : ControllerBase
+{
+    private readonly ILogger<AddPostController> _logger;
+    private readonly ICommandDispatcher _commeandDispatcher;
+
+    public AddPostController(ILogger<AddPostController> logger, ICommandDispatcher commeandDispatcher)
+    {
+        _logger = logger;
+        _commeandDispatcher = commeandDispatcher;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddPostAsync(AddPostCommand addPostCommand)
+    {
+        try
+        {
+            addPostCommand.Id = Guid.NewGuid();
+
+            await _commeandDispatcher.SendAsync(addPostCommand);
+
+            return StatusCode(StatusCodes.Status201Created, new AddPostResponse
+            {
+                Id = addPostCommand.Id,
+                Message = "New post added successfully!"
+            });
+        }
+
+        catch (InvalidOperationException invalidOperationException)
+        {
+            _logger.Log(LogLevel.Warning, invalidOperationException, "Client made a bad request!");
+
+            return BadRequest(new BaseResponse
+            {
+                Message = invalidOperationException.Message
+            });
+        }
+        catch (Exception exception)
+        {
+            const string SAFE_ERROR_MESSAGE = "An error occured while adding the new post";
+            _logger.Log(LogLevel.Error, exception, SAFE_ERROR_MESSAGE);
+
+            return StatusCode(StatusCodes.Status500InternalServerError, new AddPostResponse
+            {
+                Message = SAFE_ERROR_MESSAGE
+            });
+        }
+    }
+}
+
---
+++ Src/SM-Post/Post.Cmd/Post.Cmd.Api/Dtos/AddPostResponse.cs
@@ -0,0 +1,8 @@
+namespace Post.Cmd.Api.Dtos;
+
+using Post.Common.Dtos;
+
+public class AddPostResponse : BaseResponse
+{
+    public Guid Id { get; set; }
+}
